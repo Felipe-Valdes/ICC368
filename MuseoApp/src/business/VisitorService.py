68d39d200f27777c17a0d55f9377f3169e30fcae
333-333-333 <d.millar04@ufromail.cl>
2024-06-02 16:02:05 -0400

    add: Requerimientos a capa de persistencia (Menos 4 y 6)

--- MuseoApp/src/business/VisitorService.py
+++ MuseoApp/src/business/VisitorService.py
@@ -5,6 +5,7 @@ class VisitorService:
     def __init__(self):
         self.visitor_repository = VisitorRepository.VisitorRepository()
 
+    # Requerimiento 1
     # Obtiene a los visitantes, juntos a sus recorridos por su exhibición, en un rango de fechas y ordenados por fecha de inicio.
     # Posteriormente, transforma el resultado a json.
     def get_json_visitors_and_exhibition_between_dates_ordered_by_start_date(self, start_date, end_date):
@@ -12,6 +13,7 @@ class VisitorService:
         # Parsea los datos a json
         return visitors.to_json(orient='records')
     
+    # Requerimiento 4
     # Obtiene a los visitantes ordenados por cantidad de asistencias.
     # Posteriormente, transforma el resultado a json.
     def get_json_visitors_ordered_by_number_of_attendances(self):
@@ -19,6 +21,7 @@ class VisitorService:
         # Parsea los datos a json
         return visitors.to_json(orient='records')
     
+    # Requerimiento 6
     # Obtiene a los visitantes en un rango de fechas.
     # Posteriormente, transforma el resultado a json.
     def get_json_visitors_by_date_range(self, start_date, end_date):
--- MuseoApp/src/persistence/ArtefactRepository.py
+++ MuseoApp/src/persistence/ArtefactRepository.py
@@ -6,20 +6,8 @@ class ArtefactRepository:
 
     def __init__(self):
         self.engine = create_engine(CONNECTION_STRING)
-
-    # Requerimiento 5
-    # Obtiene a los artefactos, juntos a su familia, ordenados por familia y nombre.
-    def get_artefacts_ordered_by_family_and_name(self):
-        query = """
-        SELECT A.*, F.nombre AS Familia
-            FROM Artefacto A
-        JOIN Artefacto_Familia AF ON A.id = AF.Artefactoid
-        JOIN Familia F ON AF.Familiaid = F.id
-            ORDER BY F.nombre, A.nombre;
-        """
-        return pd.read_sql_query(query, self.engine)
     
-    # Requerimiento 4, sin filtros
+    # Requerimiento 3, sin filtros
     # Obtiene a los artefactos, juntos a sus exhibiciones.
     def get_artefacts_and_exhibitions(self):
         query = """
@@ -39,7 +27,7 @@ class ArtefactRepository:
         """
         return pd.read_sql_query(query, self.engine)
 
-    # Requerimiento 4, con filtro de id de artefacto 
+    # Requerimiento 3, con filtro de id de artefacto 
     # Obtiene a los artefactos, juntos a sus exhibiciones, filtrando por id de artefacto.
     def get_artefacts_and_exhibitions_by_artefact_id(self, artefact_id):
         query = """
@@ -61,7 +49,7 @@ class ArtefactRepository:
         """
         return pd.read_sql_query(query, self.engine, params=(artefact_id,))
     
-    # Requerimiento 4, con filtro de rango de fechas
+    # Requerimiento 3, con filtro de rango de fechas
     # Obtiene a los artefactos, juntos a sus exhibiciones, en un rango de fechas.
     def get_artefacts_and_exhibitions_between_dates(self, start_date, end_date):
         query = """
@@ -83,7 +71,7 @@ class ArtefactRepository:
         """
         return pd.read_sql_query(query, self.engine, params=(start_date, end_date))
     
-    # Requerimiento 4, con filtro de rango de fechas y id de artefacto
+    # Requerimiento 3, con filtro de rango de fechas y id de artefacto
     # Obtiene a los artefactos, juntos a sus exhibiciones, en un rango de fechas y filtrando por id de artefacto.
     def get_artefacts_and_exhibitions_between_dates_by_artefact_id(self, start_date, end_date, artefact_id):
         query = """
@@ -105,3 +93,16 @@ class ArtefactRepository:
             E.fechaInicio;
         """
         return pd.read_sql_query(query, self.engine, params=(start_date, end_date, artefact_id))
+    
+    
+    # Requerimiento 5
+    # Obtiene a los artefactos, juntos a su familia, ordenados por familia y nombre.
+    def get_artefacts_ordered_by_family_and_name(self):
+        query = """
+        SELECT A.*, F.nombre AS Familia
+            FROM Artefacto A
+        JOIN Artefacto_Familia AF ON A.id = AF.Artefactoid
+        JOIN Familia F ON AF.Familiaid = F.id
+            ORDER BY F.nombre, A.nombre;
+        """
+        return pd.read_sql_query(query, self.engine)
