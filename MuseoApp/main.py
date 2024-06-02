b94adb9931d9602333e8a9cd75089469b2421122
Felipe-Valdes <f.valdes02@ufromail.cl>
2024-06-02 17:35:21 -0400

    Cambios estructurales menores

--- MuseoApp/main.py
+++ MuseoApp/main.py
@@ -1,13 +1,1 @@
 # main.py
-import src.business.VisitorService as VisitorService
-
-if __name__ == '__main__':
-    # Define dos fecha de inicio y término, y obtiene los visitantes en ese rango
-    start_date = '2021-01-01'
-    end_date = '2021-12-31'
-
-    museum_service = VisitorService.VisitorService()
-    visitors = museum_service.get_visitors_and_exhibition_between_dates_ordered_by_start_date(start_date, end_date)
-
-    # Imprime los visitantes
-    print(visitors)
